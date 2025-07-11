82d1b5ee97f371ab18c3619ecf781e5e49ed9f41
Tellomen <113264826+Tellomen@users.noreply.github.com>
2025-07-11 15:52:37 -0500

    Update induccion_mas_bienestar_integrado.py

--- induccion_mas_bienestar_integrado.py
+++ induccion_mas_bienestar_integrado.py
@@ -5,9 +5,8 @@ import streamlit.components.v1 as components  # Para incrustar Genially
 st.set_page_config(page_title="Inducción Más Bienestar", layout="wide")
 
 # URL directa a la imagen desde GitHub (ajusta si es necesario)
-background_url = "https://drive.google.com/uc?export=view&id=16GSPB6SYsuXgPyMDyq_eGjPOjTXgAjlP""
+background_url = "https://drive.google.com/uc?export=view&id=16GSPB6SYsuXgPyMDyq_eGjPOjTXgAjlP"
 
-# Estilos con imagen de fondo desde URL
 st.markdown(f"""
     <style>
     .stApp {{
