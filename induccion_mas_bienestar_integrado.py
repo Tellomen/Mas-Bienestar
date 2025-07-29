33425e529429c644503f1cdbd94156c7ba7bdb5a
Tellomen <113264826+Tellomen@users.noreply.github.com>
2025-07-28 21:25:27 -0500

    Update induccion_mas_bienestar_integrado.py

--- induccion_mas_bienestar_integrado.py
+++ induccion_mas_bienestar_integrado.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
-from pydub import AudioSegment
 from io import BytesIO
 
 # ------------------------- CONFIGURACIÓN DE PÁGINA --------------------------
